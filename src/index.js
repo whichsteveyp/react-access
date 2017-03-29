ba015ad627975944f6f87e78f5941249b24e8d8f
Stephen Rivas Jr <srivas@netflix.com>
2017-03-29 07:47:11 -0700

    entry for default/named exports

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactAccessContext from './access-context';
+import RequireForAccess from './require-for-access';
+
+export {
+  ReactAccessContext,
+  RequireForAccess,
+};
+
+export default RequireForAccess;
