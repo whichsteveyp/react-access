e1809d798ef5afe9aee53ac090da851a3d263679
Stephen Rivas Jr <srivas@netflix.com>
2017-03-29 07:47:00 -0700

    initial copypasta for ReactAccessContext wrapper

---
+++ src/react-access-context.js
@@ -0,0 +1,43 @@
+import {Component, PropTypes} from 'react';
+import _ from 'lodash';
+
+export default class ReactAccessContext extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    userPermissions: PropTypes.arrayOf(PropTypes.string),
+    validator: PropTypes.func,
+  }
+
+  render() {
+    return this.props.children;
+  }
+
+  getChildContext = () => ({
+    authorizeAccess: this.authorizeAccess
+  })
+
+  authorizeAccess = (requiredPermissions, requireAll) => {
+    const {userPermissions, validator} = this.props;
+    return validator(userPermissions, requiredPermissions, requireAll);
+  }
+
+  static childContextTypes = {
+    authorizeAccess: PropTypes.func,
+  }
+
+  static defaultProps = {
+    validator(userPermissions, requiredPermissions, requireAll) {
+      if (requireAll === true) {
+        // TODO: userPermissions contains all requiredPermissions
+        // return
+      } else {
+        // by default we will authorize access if any of the permissions in
+        // requiredPermissions match any userPermissions
+        // TODO: is there a smaller comparision function we can use to reduce
+        // bundle size and drop a dependency?
+        return _.intersection(userCapabilities, requiredRoles).length > 1;
+      }
+    },
+    userPermissions: [],
+  }
+}
