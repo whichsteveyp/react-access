3c1807e9e9757c0e1b2ee85564b024f33d01a9af
Stephen Rivas Jr <srivas@netflix.com>
2017-03-29 07:44:55 -0700

    initial copypasta for RequireForAccess render HOC

---
+++ src/require-for-access.js
@@ -0,0 +1,29 @@
+import React, {Component, PropTypes} from 'react';
+
+export default class RequireForAccess extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    permissions: PropTypes.arrayOf(PropTypes.string),
+    invalidAccessComponent: PropTypes.node,
+    requireAll: PropTypes.bool,
+  }
+
+  static contextTypes = {
+    authorizeAccess: PropTypes.func,
+  }
+
+  render() {
+    const {children, permissions, invalidAccessComponent, requireAll} = this.props;
+    const {authorizeAccess} = this.context;
+    const pass = authorizeAccess(permissions, requireAll);
+
+    return <div className={`react-access-${pass ? '' : 'in'}valid`}>
+      {pass ? children : invalidAccessComponent}
+    </div>;
+  }
+
+  static defaultProps = {
+    invalidAccessComponent: null,
+    requireAll: false,
+  }
+}
