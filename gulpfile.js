2e322e3b96aecec272a14780a42ab4a81137c9f7
Stephen Rivas Jr <srivas@netflix.com>
2017-03-29 20:01:07 -0700

    instrument gulp & build task (copied rsr from @jmaes) for dist

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+// I have VERY LARGELY copied this from @jmeas, for...science. to start. but organized my way.
+const gulp = require('gulp');
+
+/*
+  Section: dist
+  Functions found in `gulp-tasks/dist.js` that primarily pertain to all things `dist/`,
+  which is the primary output target for this module that consumers need.
+*/
+
+const dist = require('./gulp-tasks/dist');
+gulp.task('clean', dist.clean);
+gulp.task('build', ['clean'], dist.build);
