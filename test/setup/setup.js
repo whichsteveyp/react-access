c05d4fdd89d393cd918b49175d12564ec429c7f6
Stephen Rivas Jr <sprjr@me.com>
2018-01-04 09:16:15 -0800

    update test/setup to latest enzyme adapter strategy for react16

--- test/setup/setup.js
+++ test/setup/setup.js
@@ -1,3 +1,19 @@
+import { configure } from 'enzyme';
+
+// enzyme requires special adapters now that are in different modules
+// as opposed to different imports, so we need to configure it based on
+// which React version we're testing
+if (process.env.REACT_VERSION === '^15.4.0') {
+  const Adapter = require('enzyme-adapter-react-15.4');
+  configure({ adapter: new Adapter() });
+} else if (process.env.REACT_VERSION === '<15.4.0 >15.0.0') {
+  const Adapter = require('enzyme-adapter-react-15');
+  configure({ adapter: new Adapter() });
+} else {
+  const Adapter = require('enzyme-adapter-react-16');
+  configure({ adapter: new Adapter() });
+}
+
 module.exports = function(root) {
   root = root ? root : global;
   root.expect = root.chai.expect;
