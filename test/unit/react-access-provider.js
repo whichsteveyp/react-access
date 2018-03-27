66aceace3915fca15ab20cac1f0cf728af90dc1f
Stephen Rivas Jr <sprjr@me.com>
2018-03-27 11:23:09 -0700

    fix tests for provider portion of the API

--- test/unit/react-access-provider.js
+++ test/unit/react-access-provider.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import {ReactAccessContext, __get__} from '../../src';
+import {ReactAccessProvider, __get__} from '../../src';
 
-describe('ReactAccessContext', function() {
+describe('ReactAccessProvider', function() {
   beforeEach(() => {
     this.defaultProps = {
-      userPermissions: ['regularUser'],
+      permissions: ['regularUser'],
     };
   });
 
   describe('rendering', () => {
     it('should render the provided children', () => {
-      const wrapper = shallow(<ReactAccessContext {...this.defaultProps}>
+      const wrapper = shallow(<ReactAccessProvider {...this.defaultProps}>
         <span>App Code</span>
-      </ReactAccessContext>);
+      </ReactAccessProvider>);
 
       assert(wrapper.children().length === 1);
       assert(wrapper.contains(<span>App Code</span>));
@@ -21,25 +21,25 @@ describe('ReactAccessContext', function() {
 
     it('should use the user provided validator to authorizeAccess with the expected arguments', () => {
       const validator = stub();
-      const wrapper = shallow(<ReactAccessContext {...this.defaultProps} validator={validator}>
+      const wrapper = shallow(<ReactAccessProvider {...this.defaultProps} validator={validator}>
         <span>App Code</span>
-      </ReactAccessContext>);
+      </ReactAccessProvider>);
 
       const authorizeAccess = wrapper.instance().authorizeAccess;
 
       // this method is exposed via context, and takes two arguments.
-      // it then provides the `validator` prop method with the userPermissions,
+      // it then provides the `validator` prop method with the permissions,
       // the requiredPermissions, and requireAll flag
       authorizeAccess(['edit'], false);
 
       assert(validator.calledOnce);
-      assert(validator.calledWith(this.defaultProps.userPermissions, ['edit'], false));
+      assert(validator.calledWith(this.defaultProps.permissions, ['edit'], false));
     });
 
-    it('should generically validate the userPermissions & requiredPermissions by default', () => {
-      const wrapper = shallow(<ReactAccessContext {...this.defaultProps}>
+    it('should generically validate the permissions & requiredPermissions by default', () => {
+      const wrapper = shallow(<ReactAccessProvider {...this.defaultProps}>
         <span>App Code</span>
-      </ReactAccessContext>);
+      </ReactAccessProvider>);
 
       // default props are not available on wrapper.prop() or wrapper.props()
       // so we need an instance before we can access it
